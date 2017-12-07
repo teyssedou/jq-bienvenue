5dbf53ffd23e725f795da1473b60bbf5cc19b850
teyssedou <mikaelteyssedou@gmail.com>
2017-12-07 16:28:23 +0100

    reussite de l'exercice

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+$('#bouton').click(function(){
+	var prenom = $("#first_name").val();
+	var nom = $("#last_name").val();
+	var ville = $("#city").val();
+$('#username').html(prenom+" "+nom+" "+ville)		
+})
+
 console.log('have fun !');
